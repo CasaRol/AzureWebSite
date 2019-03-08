a2b24f9ab01c9ad178dcf637103d3ae40e3e34e8
CasaRol <alexander-rol@hotmail.com>
2019-03-08 18:15:46 +0100

    Official init commit

---
+++ Assignment2_alrol17/Data/CoursesContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment2_alrol17.Models
+{
+    public class CoursesContext : DbContext
+    {
+        public CoursesContext (DbContextOptions<CoursesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Assignment2_alrol17.Models.Courses> Courses { get; set; }
+    }
+}
---
+++ Assignment2_alrol17/Models/Courses.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment2_alrol17.Models
+{
+    public class Courses
+    {
+        public int CoursesID {get; set; }
+        public String CourseName { get; set; }
+        public int Semester { get; set; }
+        public int FinalGrade { get; set; }
+
+    }
+}
